cc1c3664248cb82bcc78e250c9079dbb04cc11c9
exampleuser <exampleuser@example.com>
2016-01-21 00:30:16 +0900

    曜日を追加

--- convert.pl
+++ convert.pl
@@ -2,22 +2,29 @@
 use strict;
 use warnings;
 use YAML::Syck;
+use POSIX;
 (my $wd = __FILE__) =~ s{[^/]+\z}{};
 $wd eq "" or chdir $wd or die "ERROR: chdir '$wd': $!";
 
 my $src_dir      = "src/yaml";
 my $dst_base_dir = "dst";
 
+
+my @all_keys = qw(ymd y m d wday wday_ja wday_en name);
+my @wday_ja  = qw(日 月 火 水 木 金 土);
+my @wday_en  = qw(Sunday Monday Tuesday Wednesday Thursday Friday Saturday);
+
+
 opendir(my $d_src, $src_dir) or die "opendir '$src_dir': $!";
 my @src_files = grep {/\.yml\z/} readdir($d_src);
 closedir($d_src);
 
-my @data;
-for my $f (@src_files) {
-    my $hash = YAML::Syck::LoadFile("$src_dir/$f");
-    push @data, map { +{ ymd => $_, name => $hash->{$_} } } keys %$hash;
-}
-@data = sort { $a->{ymd} cmp $b->{ymd} } @data;
+my @data = sort { $a->{ymd} cmp $b->{ymd} }
+map {
+    my $hash = $_;
+    map { +{ ymd => $_, name => $hash->{$_} } } keys %$hash;
+} map { YAML::Syck::LoadFile("$src_dir/$_") } @src_files;
+
 my %by_year;
 for (@data) {
     $_->{ymd} =~ /^(\d+)/ or die "invalid format ymd '" . $_->{ymd} . "'";
@@ -31,7 +38,7 @@ my %outout_format = (
     yaml_long => {
         ext => "yml",
         row => sub {
-            my %v = %$_; "- { " . join(", ", map {"$_:\"$v{$_}\""} qw(ymd y m d name)) . " }\n";
+            my %v = %$_; "- { " . join(", ", map {"$_:\"$v{$_}\""} @all_keys) . " }\n";
         }
     },
     json_short => {
@@ -43,7 +50,7 @@ my %outout_format = (
         ext => "json", before => sub {"[\n"}, after => sub {"\n]\n"},
         join => sub { join(",\n", @_) },
         row => sub {
-            my %v = %$_; '{ ' . join(", ", map {"\"$_\":\"$v{$_}\""} qw(ymd y m d name)) . " }";
+            my %v = %$_; '{ ' . join(", ", map {"\"$_\":\"$v{$_}\""} @all_keys) . " }";
         }
     },
     csv_short => {
@@ -53,20 +60,20 @@ my %outout_format = (
     csv_long => {
         ext => "csv",
         row => sub {
-            my %v = %$_; join(",", map { $v{$_} } qw(ymd y m d name)) . "\n";
+            my %v = %$_; join(",", map { $v{$_} } @all_keys) . "\n";
         }
     },
     ltsv_long => {
         ext => "ltsv",
         row => sub {
-            my %v = %$_; join("\t", map {"$_:$v{$_}"} qw(ymd y m d name)) . "\n";
+            my %v = %$_; join("\t", map {"$_:$v{$_}"} @all_keys) . "\n";
         }
     },
     sql => {
         ext    => "sql",
-        before => sub {"REPLACE INTO holiday (`date`,`description`) VALUES "},
+        before => sub {"REPLACE INTO holiday (`date`,`description`) VALUES \n"},
         after  => sub {";\n"},
-        join   => sub { join ", ", @_ },
+        join   => sub { join ",\n", @_ },
         row => sub { "('" . $_->{ymd} . "','" . $_->{name} . "')" },
     },
 );
@@ -77,29 +84,46 @@ while (my ($format_name, $format_spec) = each %outout_format) {
 
     my $ext           = $format_spec->{ext};
     my $filter_row    = $format_spec->{row};
-    my $filter_before = $format_spec->{before};
-    my $filter_after  = $format_spec->{after};
+    my $filter_before = $format_spec->{before} // sub {""};
+    my $filter_after  = $format_spec->{after} // sub {""};
     my $filter_join   = $format_spec->{join} // sub { join "", @_ };
-    while (my ($year, $rows) = each %by_year) {
+
+    my @filtered_rows_all;
+
+    for my $year (sort keys %by_year) {
+        my $rows = $by_year{$year};
+
         my $dst_file = "$dst_dir/$year.$ext";
         open(my $fh, ">", $dst_file) or die "ERROR: open '$dst_file': $!";
 
-        print $fh $filter_before->() if $filter_before;
-        print $fh $filter_join->(
-            map { local $_ = $_; $filter_row->() }
-              map {
-                my $ymd = $_->{ymd};
-                ($ymd =~ /^(\d\d\d\d)-(\d\d)-(\d\d)/) or die "invalid format ymd '$ymd'";
-                +{  ymd  => $ymd,
-                    y    => int($1),
-                    m    => int($2),
-                    d    => int($3),
-                    name => $_->{name},
-                  }
-              } @$rows
-        );
-        print $fh $filter_after->() if $filter_after;
+        my @filtered_rows = map { local $_ = $_; $filter_row->() }
+          map {
+            my $ymd = $_->{ymd};
+            ($ymd =~ /^(\d\d\d\d)-(\d\d)-(\d\d)/) or die "invalid format ymd '$ymd'";
+            my ($y, $m, $d) = (int($1), int($2), int($3));
+            my (undef, undef, undef, undef, undef, undef, $wday) = localtime mktime(0, 0, 0, $d, $m - 1, $y - 1900);
+            +{  ymd     => $ymd,
+                y       => $y,
+                m       => $m,
+                d       => $d,
+                wday    => $wday,
+                wday_ja => $wday_ja[$wday],
+                wday_en => $wday_en[$wday],
+                name    => $_->{name},
+              }
+          } @$rows;
+        push @filtered_rows_all, @filtered_rows;
+
+        print $fh $filter_before->();
+        print $fh $filter_join->(@filtered_rows);
+        print $fh $filter_after->();
     }
+
+    my $dst_all_file = "$dst_dir/all.$ext";
+    open(my $fh, ">", $dst_all_file) or die "ERROR: open '$dst_all_file': $!";
+    print $fh $filter_before->();
+    print $fh $filter_join->(@filtered_rows_all);
+    print $fh $filter_after->();
 }
 
 
