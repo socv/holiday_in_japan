4ad37be4b3ae19f8084cb77f83d562c9ace064ec
socv <sockety.v@gmail.com>
2017-09-20 02:35:06 +0900

    iCalendar 書式を long と short に分ける。templateを使う

--- README_Google.md
+++ README_Google.md
@@ -3,5 +3,6 @@
 [Google カレンダー](https://calendar.google.com/calendar/) の左側に表示されてある
 「他のカレンダー」の右側の下向きボタンを押し、「URLで追加」を選択。
 
-[iCalendar形式raw](https://raw.githubusercontent.com/socv/holiday_in_japan/master/dst/iCalendar/all.ics)
-のURLを入力。
+[iCalendar short形式raw](https://raw.githubusercontent.com/socv/holiday_in_japan/master/dst/iCalendar_short/all.ics)
+[iCalendar long形式raw](https://raw.githubusercontent.com/socv/holiday_in_japan/master/dst/iCalendar_long/all.ics)
+いずれかのURLを入力。
--- convert.pl
+++ convert.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use YAML::Syck;
 use POSIX;
+use Template;
 (my $wd = __FILE__) =~ s{[^/]+\z}{};
 $wd eq "" or chdir $wd or die "ERROR: chdir '$wd': $!";
 
@@ -10,7 +11,7 @@ my $src_dir      = "src/yaml";
 my $dst_base_dir = "dst";
 
 
-my @all_keys = qw(ymd y m d wday wday_ja wday_en name reason);
+my @standard_all_keys = qw(ymd y m d wday wday_ja wday_en name reason);
 my @wday_ja  = qw(日 月 火 水 木 金 土);
 my @wday_en  = qw(Sunday Monday Tuesday Wednesday Thursday Friday Saturday);
 
@@ -48,104 +49,30 @@ for (@data) {
 }
 
 my %outout_format = (
-    yaml_short => {
-        ext => "yml", row => sub { $_->{ymd} . ": " . $_->{name} . "\n" }
-    },
-    yaml_long => {
-        ext => "yml",
-        row => sub {
-            my %v = %$_; "- { " . join(", ", map { "$_: \"$v{$_}\"" } grep { defined $v{$_} } @all_keys) . " }\n";
-        }
-    },
-    json_short => {
-        ext => "json", before => sub { "{\n" }, after => sub { "\n}\n" },
-        join => sub { join(",\n", @_) },
-        row  => sub { '"' . $_->{ymd} . '": "' . $_->{name} . '"' }
-    },
-    json_long => {
-        ext => "json", before => sub { "[\n" }, after => sub { "\n]\n" },
-        join => sub { join(",\n", @_) },
-        row  => sub {
-            my %v = %$_; '{ ' . join(", ", map { "\"$_\":\"$v{$_}\"" } grep { defined $v{$_} } @all_keys) . " }";
-        }
-    },
-    csv_short => {
-        ext => "csv",
-        row => sub { join(",", $_->{ymd}, $_->{name}) . "\n" }
-    },
-    csv_long => {
-        ext => "csv",
-        row => sub {
-            my %v = %$_; join(",", map { $v{$_} } grep { defined $v{$_} } @all_keys) . "\n";
-        }
-    },
-    ltsv_long => {
-        ext => "ltsv",
-        row => sub {
-            my %v = %$_; join("\t", map { "$_:$v{$_}" } grep { defined $v{$_} } @all_keys) . "\n";
-        }
-    },
-    sql => {
-        ext    => "sql",
-        before => sub {
-            "CREATE TABLE IF NOT EXISTS `holiday` (`date` date NOT NULL, `description` text, PRIMARY KEY (date));\n"
-              . "REPLACE INTO `holiday` (`date`,`description`) VALUES \n";
-        },
-        after => sub { ";\n" },
-        join  => sub { join ",\n", @_ },
-        row   => sub { "('" . $_->{ymd} . "','" . $_->{name} . "')" },
-    },
-    iCalendar => {
-        ext    => "ics",
-        before => sub {
-            my $ymd = POSIX::strftime("%Y%m%d", localtime);
-            return "BEGIN:VCALENDAR\n" . "VERSION:2.0\n" . "PRODID:-//socv/hiliday_in_japan//$ymd//EN\n" . join(
-                "",
-                map { "$_\n" } qw(
-                  CALSCALE:GREGORIAN
-                  METHOD:PUBLISH
-                  X-WR-CALNAME:holiday_in_japan(日本の祝日や休日)
-                  X-WR-TIMEZONE:Asia/Tokyo
-                  X-WR-CALDESC:日本の祝日や休日
-                  BEGIN:VTIMEZONE
-                  TZID:Asia/Tokyo
-                  X-LIC-LOCATION:Asia/Tokyo
-                  BEGIN:STANDARD
-                  TZOFFSETFROM:+0900
-                  TZOFFSETTO:+0900
-                  TZNAME:JST
-                  DTSTART:19700101T000000
-                  END:STANDARD
-                  END:VTIMEZONE
-                  )
-              )
-
-        },
-        after => sub { "END:VCALENDAR\n" },
-        row   => sub {
-            my %v           = %$_;
-            my $ymd_digits  = $v{ymd_digits};
-            my %label = (
-                reason => "理由",
-                reason_of_date => "日付の理由",
-                reason_of_holiday => "祝日になった理由・経緯",
-                date => "日付について",
-            );
-            my $description = join("\n\n", map { $label{$_}. ": ". $v{$_} } grep { defined $v{$_} } qw[reason reason_of_date reason_of_holiday date]);
-
-            $description =~ s/\n/\\n/gs;
-            return
-                "BEGIN:VEVENT\n"
-              . "DTSTART;VALUE=DATE:$ymd_digits\n"
-              . "DTEND;VALUE=DATE:$ymd_digits\n"
-              . "SUMMARY:$v{name}\n"
-              . ($description ? "DESCRIPTION:$description\n" : "")
-              . "END:VEVENT\n";
-        },
-    },
+    yaml_short => { ext => "yml" },
+    yaml_long => { ext => "yml" },
+    json_short => { ext => "json" },
+    json_long => { ext => "json" },
+    csv_short => { ext => "csv" },
+    csv_long => { ext => "csv" },
+    ltsv_long => { ext => "ltsv" },
+    sql => { ext => "sql" },
+    iCalendar_long => { ext    => "ics" },
+    iCalendar_short => { ext    => "ics" },
 );
 
+
+my $tt = Template->new(
+    INCLUDE_PATH => "template",
+    POST_CHOMP => 1,
+    PRE_CHOMP => 1,
+);
+
+
 while (my ($format_name, $format_spec) = each %outout_format) {
+    my $template_filename = "$format_name.tt";
+    my $template_pathname = "template/$template_filename";
+
     my $dst_dir = "$dst_base_dir/$format_name";
     (-d $dst_dir) or mkdir $dst_dir => 0755 or die "ERROR: mkdir '$dst_dir' : $!";
 
@@ -156,6 +83,7 @@ while (my ($format_name, $format_spec) = each %outout_format) {
     my $filter_join   = $format_spec->{join} // sub { join "", @_ };
 
     my @filtered_rows_all;
+    my @raw_rows_all;
 
     for my $year (sort keys %by_year) {
         my $rows = $by_year{$year};
@@ -163,8 +91,7 @@ while (my ($format_name, $format_spec) = each %outout_format) {
         my $dst_file = "$dst_dir/$year.$ext";
         open(my $fh, ">", $dst_file) or die "ERROR: open '$dst_file': $!";
 
-        my @filtered_rows = map { local $_ = $_; $filter_row->() }
-        map {
+        my @raw_rows = map {
             my $row = $_;
             my $ymd = $row->{ymd};
             ($ymd =~ /^(\d\d\d\d)-(\d\d)-(\d\d)/) or die "invalid format ymd '$ymd'";
@@ -179,21 +106,44 @@ while (my ($format_name, $format_spec) = each %outout_format) {
                 wday       => $wday,
                 wday_ja    => $wday_ja[$wday],
                 wday_en    => $wday_en[$wday],
-                (map { $_ => $row->{$_} } qw[name reason reason_of_date reason_of_holiday date])
-              }
-          } @$rows;
-        push @filtered_rows_all, @filtered_rows;
+                (map { $_ => $row->{$_} } qw[name reason purpose reason_of_date reason_of_holiday date])
+            }
+        } @$rows;
+        push @raw_rows_all, @raw_rows;
 
-        print $fh $filter_before->();
-        print $fh $filter_join->(@filtered_rows);
-        print $fh $filter_after->();
+        if(-e $template_pathname) {
+            my $vars = {
+                rows => \@raw_rows,
+                standard_all_keys => \@standard_all_keys,
+            };
+            $tt->process($template_filename, $vars, $fh) or die $tt->error . "\n";
+        }
+        else {
+            my @filtered_rows = map { local $_ = $_; $filter_row->() } @raw_rows;
+            push @filtered_rows_all, @filtered_rows;
+            print $fh $filter_before->();
+            print $fh $filter_join->(@filtered_rows);
+            print $fh $filter_after->();
+        }
     }
 
-    my $dst_all_file = "$dst_dir/all.$ext";
-    open(my $fh, ">", $dst_all_file) or die "ERROR: open '$dst_all_file': $!";
-    print $fh $filter_before->();
-    print $fh $filter_join->(@filtered_rows_all);
-    print $fh $filter_after->();
+    if(1) {
+        my $dst_all_file = "$dst_dir/all.$ext";
+        open(my $fh, ">", $dst_all_file) or die "ERROR: open '$dst_all_file': $!";
+
+        if(-e $template_pathname) {
+            my $vars = {
+                rows => \@raw_rows_all,
+                standard_all_keys => \@standard_all_keys,
+            };
+            $tt->process($template_filename, $vars, $fh) or die $tt->error . "\n";
+        }
+        else {
+            print $fh $filter_before->();
+            print $fh $filter_join->(@filtered_rows_all);
+            print $fh $filter_after->();
+        }
+    }
 }
 
 
